7f4ec34fafa0452dacba716536f2f996539f9ac5
Aaron Lieberman <aaronlcode@gmail.com>
2023-02-05 13:29:59 -0800

    Fix tree knockback, skel summoner prefab transforms

--- GameJam23/Assets/Scripts/SummonDamageHandler.cs
+++ GameJam23/Assets/Scripts/SummonDamageHandler.cs
@@ -13,6 +13,12 @@ public class SummonDamageHandler : MonoBehaviour
     [SerializeField]
     float damageCooldown = 1.5f;
 
+    [SerializeField]
+    float knockBackDistance = 0;
+
+    [SerializeField]
+    float knockBackVelocity = 10;
+
     float lastDamagedTime = float.MinValue;
 
     bool colliding;
@@ -39,8 +45,8 @@ public class SummonDamageHandler : MonoBehaviour
         Vector3 collisionDir = collision.transform.position - transform.position;
         Vector3 impulseDir = new Vector3(-collisionDir.x, 0.2f, 3).normalized;
 
-        transform.parent.position += impulseDir;
-        transform.parent.GetComponentInChildren<Rigidbody2D>().velocity = impulseDir * 10;
+        transform.parent.position += impulseDir * knockBackDistance;
+        transform.parent.GetComponentInChildren<Rigidbody2D>().velocity = impulseDir * knockBackVelocity;
         InHurtState = true;
     }
 
