4f7ac056fde573fbd8996cd458157891e8f81f80
Aaron Lieberman <aaronlcode@gmail.com>
2023-02-04 20:58:54 -0800

    Bespoke scene

--- GameJam23/Assets/TextMesh Pro/CameraController.cs
+++ GameJam23/Assets/TextMesh Pro/CameraController.cs
@@ -8,15 +8,23 @@ public class CameraController : MonoBehaviour
     [HideInInspector]
     public Transform Player;
 
-    float? _initialY;
+    bool _firstFrame = true;
+    float _initialY;
+    float _offsetY;
 
     void Update()
     {
-        _initialY ??= Player.transform.position.y;
+        // do this the first frame because otherwise Player may not have been initialized yet
+        if (_firstFrame)
+        {
+            _initialY = Player.transform.position.y;
+            _offsetY = transform.position.y - _initialY;
+            _firstFrame = false;
+        }
 
         transform.position = new Vector3(
             Player.transform.position.x,
-            _initialY.Value + (Player.transform.position.y - _initialY.Value) / 2,
+            _initialY + (Player.transform.position.y + _offsetY - _initialY) / 2,
             Player.transform.position.z - 10);
     }
 }
