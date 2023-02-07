44425d3d205f5020596d88dec858c00e29fa7f99
Aaron Lieberman <aaronlcode@gmail.com>
2023-02-06 20:18:00 -0800

    Fix bug where second playthrough (retry button) doesn't have enemies
    they were spawning but immediately dying due to init order

--- GameJam23/Assets/Scripts/EnemyResources.cs
+++ GameJam23/Assets/Scripts/EnemyResources.cs
@@ -19,7 +19,7 @@ public class EnemyResources : MonoBehaviour
 
     public UnityAction HealthChanged;
 
-    private void Start()
+    private void Awake()
     {
         Health = MaxHealth;
     }
